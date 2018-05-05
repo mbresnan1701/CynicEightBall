70903e58f585db5133223077e8362affdbfb4ad9
mbresnan1701 <bresnan.mw@gmail.com>
2018-05-05 12:20:32 -0700

    init

---
+++ app/src/main/java/com/reddogsoftware/mbresnan/cyniceightball/MainActivity.java
@@ -0,0 +1,20 @@
+package com.reddogsoftware.mbresnan.cyniceightball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+    }
+}
