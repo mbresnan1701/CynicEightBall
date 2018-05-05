f51ca37e3a2bc414c94adde0570bea9652fb2572
mbresnan1701 <bresnan.mw@gmail.com>
2018-05-05 15:27:20 -0700

    Mic input working, basic UI done

--- app/src/main/java/com/reddogsoftware/mbresnan/cyniceightball/MainActivity.java
+++ app/src/main/java/com/reddogsoftware/mbresnan/cyniceightball/MainActivity.java
@@ -1,9 +1,30 @@
 package com.reddogsoftware.mbresnan.cyniceightball;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton micButton;
+    TextView displayText;
+    SpeechRecognizer speechRecognizer;
+    Intent speechIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +37,125 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
 
+        micButton = findViewById(R.id.micButton);
+        displayText = findViewById(R.id.displayText);
+
+        micButton.setOnClickListener(onClick);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
     }
+
+    private View.OnClickListener onClick = v -> {
+        Log.d("HELLO", "ONCLICK HIT");
+        micButton.setImageResource(R.drawable.ic_mic_active);
+        initSpeechRecognizer();
+    };
+
+    private void initSpeechRecognizer() {
+
+        // Create the speech recognizer and set the listener
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this.getApplicationContext());
+        speechRecognizer.setRecognitionListener(recognitionListener);
+
+        // Create the intent with ACTION_RECOGNIZE_SPEECH
+        speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US);
+
+        listen();
+    }
+
+    public void listen() {
+
+        // startListening should be called on Main thread
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        Runnable myRunnable = () -> speechRecognizer.startListening(speechIntent);
+        mainHandler.post(myRunnable);
+    }
+
+    // This should be it's own class, but I'm lazy
+    RecognitionListener recognitionListener = new RecognitionListener() {
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.d("HELLO", "end of speech");
+            micButton.setImageResource(R.drawable.ic_mic_button);
+            displayText.setText(R.string.display_shake);
+        }
+
+        @Override
+        public void onError(int errorCode) {
+
+            // ERROR_NO_MATCH states that we didn't get any valid input from the user
+            // ERROR_SPEECH_TIMEOUT is invoked after getting many ERROR_NO_MATCH
+            // In these cases, let's restart listening.
+            // It is not recommended by Google to listen continuously user input, obviously it drains the battery as well,
+            // but for now let's ignore this warning
+            if ((errorCode == SpeechRecognizer.ERROR_NO_MATCH) || (errorCode == SpeechRecognizer.ERROR_SPEECH_TIMEOUT)) {
+
+                listen();
+            }
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+
+            // it returns 5 results as default.
+            ArrayList<String> results = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    };
 }
